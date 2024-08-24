48a991037cfbe9d37ac8e449572ab6c7069c73dc
Oscar Rico <oscar971127@gmail.com>
2024-08-24 00:05:21 -0500

    add contact

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-
-// //REFERENCIAR CARPETA GLOBAL - MIDDLEWORLD 
+// //REFERENCIAR CARPETA GLOBAL - MIDDLEWORLD - ACCEDER AL CSS E IMAGENES
 app.use(express.static(__dirname + '/public'))
 
 //ROUTER PARA INDEX
@@ -15,15 +14,22 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
+//ROUTER PARA INDEX
+app.get('/contact/', (req, res) => {
+  res.render('contact');
+})
+
+//ROUTER 404
 app.use((req, res, next) => {
   res.status(404).render('404', {
     error: 'Error 404',
-    descripcion: 'Pagina no encontrada '
+    description: 'Page not found'
   })
 })
 
 
-//ESCUCHA DEL SERVIDOR EXPRESS
+
+//ESCUCHA DEL SERVIDOR EXPRESS - IR AL FINAL
 app.listen(port, () => {
   console.log('Servidor activo en el puerto: ', port);
 })
